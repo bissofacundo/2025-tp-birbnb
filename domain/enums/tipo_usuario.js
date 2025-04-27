3aa5ec4404fa092f7175390dfcd49785f49393a0
Gaston Gonzalez Sapia <ggonzalezsapia@gmail.com>
2025-04-27 19:31:11 -0300

    se agrega metodo fromString a los enums

--- domain/enums/tipo_usuario.js
+++ domain/enums/tipo_usuario.js
@@ -1,9 +1,14 @@
 export class TipoUsuario  {
     tipo_usuario;
 
+    static fromString(token){
+        return values(TipoUsuario).find(tipo => tipo.tipo_usuario.toUpperCase() === token)
+    }
+
     constructor(tipo){
         this.tipo_usuario = tipo
     }
+
 }
 
 TipoUsuario.HUESPED = new TipoUsuario("Huesped")
