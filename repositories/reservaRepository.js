9446eecb69afd1d4899f695a5ad5a293276c44c4
LucasAponte <laponte@frba.utn.edu.ar>
2025-04-28 23:31:33 -0300

    Se agrega HealtCheck, reservaController, y metodo crearReserva

--- repositories/reservaRepository.js
+++ repositories/reservaRepository.js
@@ -11,5 +11,8 @@ export const ReservaRepository = {
 
     findReservaId(id){
         return this.reservas.find(e => e.id === id)
+    },
+    agregarReserva(nuevaReserva){
+        this.reservas.push(nuevaReserva)
     }
 }
