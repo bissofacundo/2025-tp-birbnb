870980211249be9255b9065332eec81fc45f6d30
Facundo Bisso <fbisso@frba.utn.edu.ar>
2025-06-22 02:43:32 -0300

    Le pongo algo al Home para ver como queda con contenido

--- client/src/features/home/HomePage.js
+++ client/src/features/home/HomePage.js
@@ -1,7 +1,119 @@
+import './Home.css';
+
 export const Home = () => {
     return(
         <>
-            <p>En progreso!</p>
+                    <section id="features">
+            <div class="feature">
+                <img src="https://docusaurus.io/img/undraw_typewriter.svg" />
+                <h4>Powered by MDX</h4>
+                <p>Save time and focus on text documents. Simply write docs and blog posts with MDX, and Docusaurus builds them into static HTML files ready to be served. You can even embed React components in your Markdown thanks to MDX.</p>
+            </div>
+            <div class="feature">
+                <img src="https://docusaurus.io/img/undraw_typewriter.svg" alt="" />
+                <h4>Powered by MDX</h4>
+                <p>Save time and focus on text documents. Simply write docs and blog posts with MDX, and Docusaurus builds them into static HTML files ready to be served. You can even embed React components in your Markdown thanks to MDX.</p>
+            </div>
+            <div class="feature">
+                <img src="https://docusaurus.io/img/undraw_typewriter.svg" alt="" />
+                <h4>Powered by MDX</h4>
+                <p>Save time and focus on text documents. Simply write docs and blog posts with MDX, and Docusaurus builds them into static HTML files ready to be served. You can even embed React components in your Markdown thanks to MDX.</p>
+            </div>
+            <div class="feature">
+                <img src="https://docusaurus.io/img/undraw_typewriter.svg" alt="" />
+                <h4>Powered by MDX</h4>
+                <p>Save time and focus on text documents. Simply write docs and blog posts with MDX, and Docusaurus builds them into static HTML files ready to be served. You can even embed React components in your Markdown thanks to MDX.</p>
+            </div>
+            <div class="feature">
+                <img src="https://docusaurus.io/img/undraw_typewriter.svg" alt="" />
+                <h4>Powered by MDX</h4>
+                <p>Save time and focus on text documents. Simply write docs and blog posts with MDX, and Docusaurus builds them into static HTML files ready to be served. You can even embed React components in your Markdown thanks to MDX.</p>
+            </div>
+        </section>
+        <section id="video">
+            <h3>Check it out in the intro video</h3>
+            <div class="iframe-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/_An9EsKPhp0?si=9_PSPtAhYMl36pyc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </section>
+        <section id="social-proof">
+            <h3>Loved by many engineers</h3>
+            <div class="tweets">
+                <div class="columna">
+                    <article class="card">
+                        <header>
+                            <img class="avatar" src="https://unavatar.io/x/paularmstrong?fallback=https://github.com/paularmstrong.png"/>
+                            <div>
+                                <h4>Mark Erikson</h4>
+                                <p class="handle">@acemarke</p>
+                            </div>
+                        </header>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <footer>
+                            <p>Oct 26, 2021</p>
+                        </footer>
+                    </article>
+                    <article class="card">
+                        <header>
+                            <img class="avatar" src="https://unavatar.io/x/paularmstrong?fallback=https://github.com/paularmstrong.png"/>
+                            <div>
+                                <h4>Mark Erikson</h4>
+                                <p class="handle">@acemarke</p>
+                            </div>
+                        </header>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <footer>
+                            <p>Oct 26, 2021</p>
+                        </footer>
+                    </article>
+                </div>
+                <div class="columna">
+                    <article class="card">
+                        <header>
+                            <img class="avatar" src="https://unavatar.io/x/paularmstrong?fallback=https://github.com/paularmstrong.png"/>
+                            <div>
+                                <h4>Mark Erikson</h4>
+                                <p class="handle">@acemarke</p>
+                            </div>
+                        </header>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <footer>
+                            <p>Oct 26, 2021</p>
+                        </footer>
+                    </article>
+                </div>
+                <div class="columna">
+                    <article class="card">
+                        <header>
+                            <img class="avatar" src="https://unavatar.io/x/paularmstrong?fallback=https://github.com/paularmstrong.png"/>
+                            <div>
+                                <h4>Mark Erikson</h4>
+                                <p class="handle">@acemarke</p>
+                            </div>
+                        </header>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <footer>
+                            <p>Oct 26, 2021</p>
+                        </footer>
+                    </article>
+                    <article class="card">
+                        <header>
+                            <img class="avatar" src="https://unavatar.io/x/paularmstrong?fallback=https://github.com/paularmstrong.png"/>
+                            <div>
+                                <h4>Mark Erikson</h4>
+                                <p class="handle">@acemarke</p>
+                            </div>
+                        </header>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <p>We've been using Docusaurus for all the Redux org docs sites for the last couple years, and it's great! We've been able to focus on content, customize some presentation and features, and It Just Works.</p>
+                        <footer>
+                            <p>Oct 26, 2021</p>
+                        </footer>
+                    </article>
+                </div>
+            </div>
+        </section>
         </>
     )
 
