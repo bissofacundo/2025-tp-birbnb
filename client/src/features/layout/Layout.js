4b3cf0d85ea8ed4d69ee111c92df466fec194146
Facundo Bisso <fbisso@frba.utn.edu.ar>
2025-06-22 02:43:17 -0300

    Agrego el Footer

--- client/src/features/layout/Layout.js
+++ client/src/features/layout/Layout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import NavBar from "../../components/navbar/NavBar.js";
+import Footer from "../../components/footer/Footer.js";
 
 export const Layout = () => {
     return(
         <>
             <NavBar></NavBar>
             <Outlet />
+            <Footer></Footer>
         </>
     )
 }
