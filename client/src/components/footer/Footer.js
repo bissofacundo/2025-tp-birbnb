5ed52687293e0b047c822e2b95cc92b572cfca0d
Facundo Bisso <fbisso@frba.utn.edu.ar>
2025-06-23 19:50:30 -0300

    Separo estilos en un archivo aparte y elimino componentes estilados del Footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,55 +1,39 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Select from '@mui/joy/Select';
 import Option from '@mui/joy/Option';
 import LanguageIcon from '@mui/icons-material/Language';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
+import './Footer.css'
 
 function Footer() {
   return (
-    <Box
-        component="footer"
-        sx={{
-            py: 3,
-            px: 2,
-            position: 'fixed',
-            bottom: '0',
-            width: '100%',
-            height: '0.5vh',
-            display: 'flex',
-            alignItems: 'center',
-            paddingLeft: "0",
-            backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-        }}
-        >
-        <Typography variant="body1" align="left" paddingLeft="5vw" fontFamily="Inter" marginRight= 'auto'>
-            © {new Date().getFullYear()} Birbnb, Inc.
-        </Typography>
-        <div style={{display:'flex', alignItems: 'center', paddingRight: '5vw', gap: '10px'}}>
-        <Select defaultValue="Español (AR)" indicator="" startDecorator={<LanguageIcon/>} variant="standard" disableUnderline="true" sx={{ backgroundColor: "inherit"}} >
-            <Option value="Español (AR)" >Español (AR)</Option>
-            <Option value="English (US)">English (US)</Option>
-            <Option value="Français (FR)">Français (FR)</Option>
-            <Option value="Italiano (IT)">Italiano (IT)</Option>
-        </Select>        
-        <Select defaultValue="$ ARS" indicator="" variant="standard" disableUnderline="true" sx={{ backgroundColor: "inherit"}} >
-            <Option value="$ ARS">$ ARS</Option>
-            <Option value="$ USD">$ USD</Option>
-            <Option value="€ EUR">€ EUR</Option>
-        </Select>
-        <div style={{display:'flex', alignItems: 'center', gap: '5px', paddingLeft: '0.75rem'}}>
-        <FacebookIcon />
-        <InstagramIcon />
-        <XIcon />
+    <>
+    <footer className="footer">
+        <div className='copyright'>
+            <p>© {new Date().getFullYear()} Birbnb, Inc.</p>
         </div>
+        <div className="footer-right">
+            <Select defaultValue="Español (AR)" indicator="" startDecorator={<LanguageIcon/>} variant="standard" disableUnderline="true" className='language-select' >
+                <Option value="Español (AR)" >Español (AR)</Option>
+                <Option value="English (US)">English (US)</Option>
+                <Option value="Français (FR)">Français (FR)</Option>
+                <Option value="Italiano (IT)">Italiano (IT)</Option>
+            </Select>        
+            <Select defaultValue="$ ARS" indicator="" variant="standard" disableUnderline="true" className="coin-select" >
+                <Option value="$ ARS">$ ARS</Option>
+                <Option value="$ USD">$ USD</Option>
+                <Option value="€ EUR">€ EUR</Option>
+            </Select>
+            <div className='social-icons'>
+                <FacebookIcon />
+                <InstagramIcon />
+                <XIcon />
+            </div>
         </div>
-    </Box>
+    </footer>
+    </>
   );
 }
 
