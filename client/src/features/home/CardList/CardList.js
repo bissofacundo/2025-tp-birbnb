0c20a52f38ace6d95d942114f1280e18c0c49bcb
SimonCutreraUTN <scutrera@frba.utn.edu.ar>
2025-06-23 12:43:10 -0300

    separo card de cardList

--- client/src/features/home/CardList/CardList.js
+++ client/src/features/home/CardList/CardList.js
@@ -5,58 +5,68 @@ import {useNavigate} from "react-router";
 import './CardList.css'
 
 
-export const CardList = ({alojamientos}) => {
-    
+const BirbnbCard = ({nombre, anfitrion, fotos, direccion, cantHuespedesMax, precioPorNoche, caracteristicas, id}) => {
+
     const navigate = useNavigate();
-    
+
     const alVerMas = (id) => {
         //alert('clicked');
         navigate(`/alojamientos/${id}`);
         //navigate(`/alojamientos/1`);
     }
-//console.log(alojamientos.map((alojamiento) =>alojamiento.fotos[0].path))
 
-return(
-    <div class="cardlist">{
-        alojamientos.map((alojamiento) => 
-            <Card class= 'cardAlojamiento' color="primary" variant='outlined' sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }} >
-                <div class="cardHeader">
-                    <content>
-                        <h3 class="nombre">{alojamiento.nombre}</h3> 
-                        <p class="anfitrion">{alojamiento.anfitrion}</p>
-                    </content>
-                </div>
-                <CardOverflow sx={{width: '100%'}}>
-                    <AspectRatio sx={{ minWidth: 200, width:'100%'}}>
-                    <img class="imagenAlojamiento"
-                        src={alojamiento.fotos[0].path}
-                        srcSet=""
-                        loading="lazy"
-                        alt={alojamiento.fotos[0].descripcion}
-                    />
-                    </AspectRatio>
-                </CardOverflow>
-                <div class="cardDescripcion">
-                    <div class="cardText"> 
-                        <div class="ubicacion">
-                            <p>Ciudad: {alojamiento.direccion.ciudad} </p>    
-                            <p>Pais: {alojamiento.direccion.pais}</p>
-                        </div>
-                        <div class="detalles">
-                            <p>Cantidad de Huspedes: {alojamiento.cantHuespedesMax}</p>
-                            <p>Precio por noche: ${alojamiento.precioPorNoche}</p>
-                            <p>Caracteristicas: {alojamiento.caracteristicas.map(caracterista => caracterista.toLowerCase()).join(", ")}</p>
-                        </div>
+    return(
+        <Card class= 'cardAlojamiento' color="primary" variant='outlined' sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }} >
+            <div class="cardHeader">
+                <content>
+                    <h3 class="nombre">{nombre}</h3> 
+                    <p class="anfitrion">{anfitrion}</p>
+                </content>
+            </div>
+            <CardOverflow sx={{width: '100%'}}>
+                <AspectRatio sx={{ minWidth: 200, width:'100%'}}>
+                <img class="imagenAlojamiento"
+                    src={fotos[0].path}
+                    srcSet=""
+                    loading="lazy"
+                    alt={fotos[0].descripcion}
+                />
+                </AspectRatio>
+            </CardOverflow>
+            <div class="cardDescripcion">
+                <div class="cardText"> 
+                    <div class="ubicacion">
+                        <p>Ciudad: {direccion.ciudad} </p>    
+                        <p>Pais: {direccion.pais}</p>
                     </div>
-                    <div class="cardButton">   
-                        <CardOverflow>
-                            <Button variant="contained" color="primary" size="md" onClick={() => alVerMas(alojamiento._id)}>
-                                ver mas +
-                            </Button>
-                        </CardOverflow>
+                    <div class="detalles">
+                        <p>Cantidad de Huspedes: {cantHuespedesMax}</p>
+                        <p>Precio por noche: ${precioPorNoche}</p>
+                        <p>Caracteristicas: {caracteristicas.map(caracterista => caracterista.toLowerCase()).join(", ")}</p>
                     </div>
                 </div>
-            </Card>
+                <div class="cardButton">   
+                    <CardOverflow>
+                        <Button variant="contained" color="primary" size="md" onClick={() => alVerMas(id)}>
+                            ver mas +
+                        </Button>
+                    </CardOverflow>
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+export const CardList = ({alojamientos}) => {
+//console.log(alojamientos)
+
+    return(
+        <div class="cardlist">{
+            alojamientos.map((alojamiento) => 
+                <BirbnbCard nombre={alojamiento.nombre} anfitrion={alojamiento.anfitrion} fotos={alojamiento.fotos} direccion={alojamiento.direccion}
+                            cantHuespedesMax={alojamiento.cantHuespedesMax} precioPorNoche={alojamiento.precioPorNoche} caracteristicas={alojamiento.caracteristicas}
+                            id={alojamiento._id}>
+                </BirbnbCard>
             )}
         </div>
     )
