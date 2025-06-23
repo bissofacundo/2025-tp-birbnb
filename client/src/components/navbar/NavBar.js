d58e2e40e046370f243d6b4c37fc9749e41373b5
Facundo Bisso <fbisso@frba.utn.edu.ar>
2025-06-23 20:53:57 -0300

    Separo estilos en un archivo aparte y elimino componentes estilados del appbar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,150 +1,43 @@
 import * as React from 'react';
-import { alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import Button from '@mui/material/Button';
+import './NavBar.css'
 
 const pages = ['Alojamientos', 'Reservas'];
 
 export default function NavBar() {
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-    </Menu>
-  );
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar sx={{ backgroundColor: "#FAFAFA" }}>
-        <div style={{display: "flex", paddingLeft: "1vw", alignItems: "center"}}>
-        <img src={process.env.PUBLIC_URL + '/birbnb_logo.png'} height= "30px" width= "30px" alt="" />
-          <Typography
-            variant="h4"
-            noWrap
-            component="div"
-            color="black"
-            sx={{ display: { xs: 'none', sm: 'block' }, fontFamily: 'Inter', fontWeight: 'medium' }}
-          >
-            <span style={{color: '#D055BB'}}>ir</span>bnb
-          </Typography>
+        <Toolbar className="toolbar" >
+          <div className="logo">
+            <img src={process.env.PUBLIC_URL + '/birbnb_logo.png'} id="logo" alt="" />
+            <h4 className='logo-text'>
+              <span>ir</span>bnb
+            </h4>
           </div>
-          <Box sx={{ flexGrow: 2 }} />
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, gap: "100px" }}>
+          <div  className="appbar-buttons" >
             {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: 'black', display: 'block', fontFamily: 'inter', textTransform: 'capitalize', fontSize: "20px", '&:hover': { backgroundColor: alpha('#D055BB', '0.25') }  }}
-              >
+              <Button key={page} id="button" >
                 {page}
               </Button>
             ))}
-          </Box>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, paddingRight: "1vw" }}>
-            <IconButton size="large" aria-label="home" color="black" sx={{ '&:hover': { backgroundColor: alpha('#D055BB', '0.25') } }}>
+          </div>
+          <div className="appbar-icons" >
+            <IconButton size="large" aria-label="home" color="black" id="appbar-icon" >
               <HomeOutlinedIcon />
             </IconButton>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="black"
-              sx={{ '&:hover': { backgroundColor: alpha('#D055BB', '0.25') } }}
+              id="appbar-icon"
             >
               <Badge badgeContent={17} color="error">
                 <NotificationsNoneOutlinedIcon />
@@ -154,31 +47,14 @@ export default function NavBar() {
               size="large"
               edge="end"
               aria-label="account of current user"
-              aria-controls={menuId}
               aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
               color="black"
-              sx={{ '&:hover': { backgroundColor: alpha('#D055BB', '0.25') } }}
+              id="appbar-icon"
             >
               <AccountCircle />
             </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="black"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
+          </div>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
   );
 }
